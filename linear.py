845fb66ad470ef3c5660a11b73b96cac8da71336
sdatla2 <sdatla2@illinois.edu>
2018-04-06 19:28:24 +0100

    Updating linear regression

--- linear.py
+++ linear.py
@@ -8,23 +8,22 @@ class LinearRegression:
         self.steps = steps
 
     def fit(self,input,labels,disp=False):
-        X = np.insert(input,0,1,axis=1)
         input = self.normalise(input)
-        self.weights = np.ones(len(X[0]))
+        X = np.insert(input,0,1,axis=1)
+        Y = [y for y,_ in sorted(zip(labels,X))]
+        X = [x for _,x in sorted(zip(labels,X))]
+        weights = np.zeros(len(X[0]))
         for i in range(0,self.steps):
-            X_perm, Y_perm = self.permute(X,labels)
-            for j in range(0,len(X)):
-                y_hat = np.dot(X_perm[j],self.weights)
-                error = Y_perm[j] - y_hat
-                print error
-                update = np.dot(error.T,X_perm[j])
-                print update
-                self.weights += self.lr * np.dot(error.T,X_perm[j])
+            y_hat = np.dot(X,weights)
+            error = Y - y_hat
+            print error
+            weights += self.lr * np.dot(error.T,X)
+        self.weights = weights
         return self
 
-    def permute(self,inputs,labels):
-        perm_indices = np.random.permutation(len(labels))
-        return inputs[perm_indices], labels[perm_indices]
+    # def permute(self,inputs,labels):
+    #     perm_indices = np.random.permutation(len(labels))
+    #     return inputs[perm_indices], labels[perm_indices]
 
     def cost(self,error,num_samples):
         return np.sum(error ** 2) / (2 * num_samples)
